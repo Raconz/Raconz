b408a269622128524d94dbc599ae2cb51ef10bbc
Raconz <55070143+Raconz@users.noreply.github.com>
2019-09-09 09:35:50 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>WonoFarm</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('semantic/semantic.min.css')?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('semantic/semantic.css')?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('semantic/slider.css')?>">
+	<script src="<?php echo base_url('semantic/jquery.min.js')?>" type="text/javascript"></script>
+	<script src="<?php echo base_url('semantic/semantic.min.js')?>" type="text/javascript"></script>
+	<script src="<?php echo base_url('semantic/semantic.js')?>" type="text/javascript"></script>
+</head>
+<body class="Site">
+<header class="ui grid">
+		<div class="slider">
+			<figure>
+				<img src="<?php echo base_url('semantic/images/wireframe/paragraph.png') ?>" style="width: 20%; height: 100px">
+				<img src="<?php echo base_url('semantic/images/wireframe/paragraph.png') ?>" style="width: 20%; height: 100px">
+				<img src="<?php echo base_url('semantic/images/wireframe/paragraph.png') ?>" style="width: 20%; height: 100px">
+				<img src="<?php echo base_url('semantic/images/wireframe/paragraph.png') ?>" style="width: 20%; height: 100px">
+				<img src="<?php echo base_url('semantic/images/wireframe/paragraph.png') ?>" style="width: 20%; height: 100px">
+			</figure>
+		</div>
+		<div class="ui six item stackable tabs inverted menu">
+			<a href="" class="item">HOME</a>
+			<a href="" class="item">BISNIS</a>
+			<a href="" class="item">EDUCATION</a>
+			<a href="" class="item">PROFILE</a>
+			<a href="" class="item">GALERI</a>
+			<a href="" class="item">LOGIN</a>
+		</div>
+</header>
---
+++ index.php
@@ -0,0 +1,73 @@
+<div class="ui grid container segment stackable">
+	<div class="twelve wide column">
+		<h3 class="ui dividing center aligned header">Produk</h3>
+		<div class="ui doubling five cards">
+			<div class="ui card">
+				<a href=""><img class="ui medium image" src="<?php echo base_url('semantic/images/wireframe/image.png') ?>"></a>
+				<div class="content">
+					$00.00
+					<div class="description">
+						Nama Barang
+					</div>
+				</div>
+			</div>
+			<div class="ui card">
+				<a href=""><img class="ui medium image" src="<?php echo base_url('semantic/images/wireframe/image.png') ?>"></a>
+				<div class="content">
+					$00.00
+					<div class="description">
+						Nama Barang
+					</div>
+				</div>
+			</div>
+			<div class="ui card">
+				<a href=""><img class="ui medium image" src="<?php echo base_url('semantic/images/wireframe/image.png') ?>"></a>
+				<div class="content">
+					$00.00
+					<div class="description">
+						Nama Barang
+					</div>
+				</div>
+			</div>
+			<div class="ui card">
+				<a href=""><img class="ui medium image" src="<?php echo base_url('semantic/images/wireframe/image.png') ?>"></a>
+				<div class="content">
+					$00.00
+					<div class="description">
+						Nama Barang
+					</div>
+				</div>
+			</div>
+			<div class="ui card">
+				<a href=""><img class="ui medium image" src="<?php echo base_url('semantic/images/wireframe/image.png') ?>"></a>
+				<div class="content">
+					$00.00
+					<div class="description">
+						Nama Barang
+					</div>
+				</div>
+			</div>
+		</div>
+		<br>
+		<div class="ui grid center aligned">
+			<button class="ui green button">Ke Buisnes</button>
+		</div>
+		<br>
+		<table class="ui selectable celled table">
+			<thead class="center aligned">
+				<tr>
+					<th>Arsip Artikel</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr><td>data</td></tr>
+				<tr><td>data</td></tr>
+				<tr><td>data</td></tr>
+			</tbody>
+		</table>
+		<br>
+		<div class="ui grid center aligned">
+			<button class="ui green button">Ke Education</button>
+		</div>
+		<br>
+	</div>
