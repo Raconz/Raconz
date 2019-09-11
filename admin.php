cc83b94e7d7ee67fec4a30e8313442af903d3e80
Raconz <55070143+Raconz@users.noreply.github.com>
2019-09-11 11:55:34 +0700

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,104 @@
+<div class="ui fourteen wide column">
+	<table class="ui celled table equal">
+		<h3 class="ui dividing center aligned header">Daftar Admin</h3>
+		<thead class="ui center aligned">
+			<tr>
+				<th>Nama</th>
+				<th>Username</th>
+				<th>Jenis Kelamin</th>
+				<th>Alamat</th>
+				<th>Contact</th>
+				<th>Foto</th>
+				<th>Foto KTP</th>
+				<th>Aksi</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Ozzi</td>
+				<td>racconz</td>
+				<td>laki-laki</td>
+				<td>Watumalang</td>
+				<td>089789678556</td>
+				<td>img</td>
+				<td>img</td>
+				<td><button type="" class="ui red button">X</button></td>
+			</tr>
+		</tbody>	
+	</table><br>
+	<div class="ui grid center aligned">
+			<button class="ui primary button" style="width: 20%">Tambah</button>
+	</div>
+<br><br>
+	<table  class="ui celled table equal">
+		<h3 class="ui dividing center aligned header">Daftar Member</h3>
+		<thead class="ui center aligned">
+			<tr>
+				<th>Nama</th>
+				<th>Username</th>
+				<th>Jenis Kelamin</th>
+				<th>Alamat</th>
+				<th>Contact</th>
+				<th>Foto</th>
+				<th>Foto KTP</th>
+				<th>Aksi</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>nana</td>
+				<td>nanass</td>
+				<td>perempuan</td>
+				<td>Kejajar</td>
+				<td>089789678556</td>
+				<td>img</td>
+				<td>img</td>
+				<td>
+					<button type="" class="ui red button">X</button>
+					<button type="" class="ui black button">Band</button>
+				</td>
+			</tr>
+		</tbody>	
+	</table><br>
+	<div class="ui grid center aligned">
+			<button class="ui primary button" style="width: 20%">Tambah</button>
+	</div>
+	</table>
+<br><br>
+	<table  class="ui celled table equal">
+		<h3 class="ui dividing center aligned header">Daftar Supplier</h3>
+		<thead class="ui center aligned">
+			<tr>
+				<th>Nama</th>
+				<th>Username</th>
+				<th>Jenis Kelamin</th>
+				<th>Alamat</th>
+				<th>Contact</th>
+				<th>Foto</th>
+				<th>Foto KTP</th>
+				<th>Aksi</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>tio</td>
+				<td>tioks</td>
+				<td>laki-laki</td>
+				<td>Wonosobo</td>
+				<td>089789678556</td>
+				<td>img</td>
+				<td>img</td>
+				<td>
+					<button type="" class="ui red button">X</button>
+					<button type="" class="ui black button">Band</button>
+				</td>
+			</tr>
+		</tbody>	
+	</table><br>
+	<div class="ui grid center aligned">
+			<button class="ui primary button" style="width: 20%">Tambah</button>
+	</div>
+	</table>
+
+</div>
+</div>
