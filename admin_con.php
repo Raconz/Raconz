cd4aebf813e97842e9899dc1206293f5254285db
Raconz <55070143+Raconz@users.noreply.github.com>
2019-09-12 14:53:08 +0700

    Add files via upload

--- admin_con.php
+++ admin_con.php
@@ -10,8 +10,108 @@ class Admin_con extends CI_Controller {
 		$this->load->view('admin');
 		$this->load->view('footer');
 	}
-
+	public function form_adm()
+	{
+		$this->load->view('header_ad');
+		$this->load->view('sidebar_ad');	
+		$this->load->view('form_adm');
+		$this->load->view('footer');
+	}
+	public function form_sup()
+	{
+		$this->load->view('header_ad');
+		$this->load->view('sidebar_ad');	
+		$this->load->view('form_sup');
+		$this->load->view('footer');
+	}
+	public function artikel_ad()
+	{
+		$this->load->view('header_ad');
+		$this->load->view('sidebar_ad');	
+		$this->load->view('artikel_ad');
+		$this->load->view('footer');
+	}
+	public function tambah_art()
+	{
+		$this->load->view('header_ad');
+		$this->load->view('sidebar_ad');	
+		$this->load->view('tambah_art');
+		$this->load->view('footer');
+	}
+	public function transaksi_ad()
+	{
+		$this->load->view('header_ad');
+		$this->load->view('sidebar_ad');	
+		$this->load->view('transaksi_ad');
+		$this->load->view('footer');
+	}
+	public function katbrg_ad()
+	{
+		$this->load->view('header_ad');
+		$this->load->view('sidebar_ad');	
+		$this->load->view('katbrg_ad');
+		$this->load->view('footer');
+	}
+	public function katart_ad()
+	{
+		$this->load->view('header_ad');
+		$this->load->view('sidebar_ad');	
+		$this->load->view('katart_ad');
+		$this->load->view('footer');
+	}
+	public function penyuluh_ad()
+	{
+		$this->load->view('header_ad');
+		$this->load->view('sidebar_ad');	
+		$this->load->view('penyuluh_ad');
+		$this->load->view('footer');
+	}
+	public function perpeny_ad()
+	{
+		$this->load->view('header_ad');
+		$this->load->view('sidebar_ad');	
+		$this->load->view('perpeny_ad');
+		$this->load->view('footer');
+	}
+	public function perditer_ad()
+	{
+		$this->load->view('header_ad');
+		$this->load->view('sidebar_ad');	
+		$this->load->view('perditer_ad');
+		$this->load->view('footer');
+	}
+	public function form_peny()
+	{
+		$this->load->view('header_ad');
+		$this->load->view('sidebar_ad');	
+		$this->load->view('form_peny');
+		$this->load->view('footer');
+	}
+	
 }
 
 /* End of file admin.php */
 /* Location: ./application/controllers/admin_con.php */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
