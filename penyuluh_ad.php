93b6d9ff0aa58094a9d01cedbc04e0ee6ab01d29
Raconz <55070143+Raconz@users.noreply.github.com>
2019-09-12 15:18:13 +0700

    Add files via upload

---
+++ penyuluh_ad.php
@@ -0,0 +1,145 @@
+<div class="ui fourteen wide column">
+	<table class="ui celled table equal">
+		<h3 class="ui dividing center aligned header">Daftar Permintaan Penyuluh</h3>
+		<thead class="ui center aligned">
+			<tr>
+				<th>Penyuluh</th>
+				<th>Peminta</th>
+				<th>Alasan</th>
+				<th>Tempat</th>
+				<th>Tanggal</th>
+				<th>Aksi</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>
+					<button type="" class="ui Primary button">Ya</button>
+					<button type="" class="ui red button">Tidak</button>
+				</td>
+			</tr>
+			<tr>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>
+					<button type="" class="ui Primary button">Ya</button>
+					<button type="" class="ui red button">Tidak</button>
+				</td>
+			</tr>
+			<tr>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>
+					<button type="" class="ui Primary button">Ya</button>
+					<button type="" class="ui red button">Tidak</button>
+				</td>
+			</tr>
+			<tr>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>
+					<button type="" class="ui Primary button">Ya</button>
+					<button type="" class="ui red button">Tidak</button>
+				</td>
+			</tr>
+			<tr>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>
+					<button type="" class="ui Primary button">Ya</button>
+					<button type="" class="ui red button">Tidak</button>
+				</td>
+			</tr>
+			
+		</tbody>
+	</table>
+<br><br>
+	<table class="ui celled table equal">
+		<h3 class="ui dividing center aligned header">Daftar Penyuluh</h3>
+		<thead class="ui center aligned">
+			<tr>
+				<th>Nama</th>
+				<th>Alamat</th>
+				<th>contact</th>
+				<th>Foto</th>
+				<th>Aksi</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>
+					<button type="" class="ui red button">X</button>
+					<button type="" class="ui primary button">E</button>
+				</td>
+			</tr>
+			<tr>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>
+					<button type="" class="ui red button">X</button>
+					<button type="" class="ui primary button">E</button>
+				</td>
+			</tr>
+			<tr>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>
+					<button type="" class="ui red button">X</button>
+					<button type="" class="ui primary button">E</button>
+				</td>
+			</tr>
+			<tr>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>
+					<button type="" class="ui red button">X</button>
+					<button type="" class="ui primary button">E</button>
+				</td>
+			</tr>
+			<tr>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>
+					<button type="" class="ui red button">X</button>
+					<button type="" class="ui primary button">E</button>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+<br><br>
+		<div class="ui grid center aligned">
+			<button class="ui primary button" style="width: 20%">Tambah Penyuluh</button>
+		</div>
+<br>
+
+</div>
+</div>
