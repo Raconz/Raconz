5d7850522d4950e67ad3dc4803c9bb404a177232
Raconz <55070143+Raconz@users.noreply.github.com>
2019-09-12 14:47:35 +0700

    Add files via upload

---
+++ form_adm.php
@@ -0,0 +1,36 @@
+	<div class="ui thirteen wide column">
+		<h3 class="ui dividing center aligned header">Tambah Admin</h3><br><br>
+		<form class="ui form">
+			<div class="field">
+		    	<label>Nama</label>
+		    	<input type="text" placeholder="" />
+		 	 </div>
+		 	 <div class="field">
+		    	<label>Username</label>
+		    	<input type="text" placeholder="" />
+		 	 </div>
+		 	 <div class="field">
+		    	<label>Jenis Kelamin</label>
+		    	<input type="text" placeholder="" />
+		 	 </div>
+		 	 <div class="field">
+		    	<label>Alamat</label>
+		    	<input type="text" placeholder="" />
+		 	 </div>
+		 	 <div class="field">
+		    	<label>Contact</label>
+		    	<input type="text" placeholder="" />
+		 	 </div>
+		 	 <div class="field">
+			    <label>Foto</label>
+			    <input type="file" placeholder="" />
+			 </div>
+			 <div class="field">
+			    <label>Foto KTP</label>
+			    <input type="file" placeholder="" />
+			 </div>
+		  <button type="submit" class="ui black button">Back</button>
+		  <button type="submit" class="ui right floated primary button">Tambah</button>
+		</form>		
+	</div>
+</div>
--- form_art.php
+++ form_art.php
@@ -1,9 +1,25 @@
 <div class="ui grid container segment stackable">
-	<div class="twelve wide column">
+	<div class="ui twelve wide column">
 		<h3 class="ui dividing center aligned header">Tambah Artikel</h3><br><br>
 		<form class="ui form">
 		  <div class="field">
+		    <label>User</label>
+		    <input type="text" placeholder="" />
+		  </div>
+		  <div class="field">
+		    <label>Kategori Artikel</label>
+		    <input type="text" placeholder="" />
+		  </div>
+		  <div class="field">
 		    <label>Judul Artikel</label>
+		    <input type="text" placeholder="" />
+		  </div>
+		  <div class="field">
+		    <label>Tanggal</label>
+		    <input type="text" placeholder="" />
+		  </div>
+		   <div class="field">
+		    <label>contact</label>
 		    <input type="text" placeholder="" />
 		  </div>
 		  <div class="field">
@@ -18,3 +34,4 @@
 		  <button type="submit" class="ui right floated primary button">Tambah</button>
 		</form>
 	</div>
+
