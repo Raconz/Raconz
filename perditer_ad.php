dba04aab207566609a23c17c6e5fe6b92869fdaf
Raconz <55070143+Raconz@users.noreply.github.com>
2019-09-12 14:46:11 +0700

    Add files via upload

---
+++ perditer_ad.php
@@ -0,0 +1,74 @@
+<div class="ui fourteen wide column">
+	<table class="ui celled table equal">
+		<h3 class="ui dividing center aligned header">Daftar Permintaan Penyuluh</h3>
+		<thead class="ui center aligned">
+			<tr>
+				<th>Penyuluh</th>
+				<th>Peminta</th>
+				<th>Alasan</th>
+				<th>Tempat</th>
+				<th>Tanggal</th>
+				<th>Aksi</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>
+					<button type="" class="ui red button">X</button>
+				</td>
+			</tr>
+			<tr>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>
+					<button type="" class="ui red button">X</button>
+				</td>
+			</tr>
+			<tr>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>
+					<button type="" class="ui red button">X</button>
+				</td>
+			</tr>
+			<tr>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>
+					<button type="" class="ui red button">X</button>
+				</td>
+			</tr>
+			<tr>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>Data</td>
+				<td>
+					<button type="" class="ui red button">X</button>
+				</td>
+			</tr>
+			
+		</tbody>
+	</table>
+<br><br>
+		<div class="ui grid center aligned">
+			<button class="ui primary button" style="width: 20%">Tambah</button>
+		</div>
+<br>
+</div>
+</div>
